c5ae392e4d67ecc6b0525ef2e125654b286dc695
David Mark <david.mark@cinsoft.biz>
2016-12-09 08:59:00 -0500

    Don't create noConflict for modular environments
    Pointless to create noConflict for modular environments.

--- src/exports/global.js
+++ src/exports/global.js
@@ -4,29 +4,31 @@ define( [
 
 "use strict";
 
-	// Copy existing global jQuery
-
-	var jQueryImported = jQuery;
-
-	// Copy existing global $
-
-	var $Imported = $;
-
-	_jQuery.noConflict = function( deep ) {
-		if ( $ === _jQuery ) {
-			$ = $Imported;
-		}
-
-		if ( deep && jQuery === _jQuery ) {
-			jQuery = jQueryImported;
-		}
-
-		return _jQuery;
-	};
-	
 	// If not restricted from exporting jQuery to global scope...
 
 	if ( !noGlobal ) {
+		
+		// Copy existing global jQuery
+
+		var jQueryImported = jQuery;
+
+		// Copy existing global $
+
+		var $Imported = $;
+
+		_jQuery.noConflict = function( deep ) {
+			if ( $ === _jQuery ) {
+				$ = $Imported;
+			}
+
+			if ( deep && jQuery === _jQuery ) {
+				jQuery = jQueryImported;
+			}
+
+			return _jQuery;
+		};
+		
+		
 		jQuery = $ = _jQuery;
 	}
 } );
